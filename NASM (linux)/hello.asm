9fab2ea6e83d2020ee86ef1a436c1ccc2cc515f0
Adam <adamchehab@gmail.com>
2021-05-24 01:34:53 +0300

    Commit

--- NASM (linux)/hello.asm
+++ NASM (linux)/hello.asm
@@ -1,19 +1,16 @@
-; эта секция остается статической во всей программе
-section	.data
-msg db 'Hello, world!', 0xa  ; содержимое строки для вывода
-len equ $ - msg              ; длина строки
-
-; секция с кодом программы
-section	.text
-   global _start    ; необходимо для линкера (ld)
-	
-_start:	            ; сообщает линкеру стартовую точку
-   mov	edx,len     ; длина строки 
-   mov	ecx,msg     ; строка
-   mov	ebx,1       ; дескриптор файла (stdout)
-   mov	eax,4       ; номер системного вызова (sys_write)
-   int	0x80        ; вызов ядра
-	
-   mov	eax,1       ; номер системного вызова (sys_exit)
-   int	0x80        ; вызов ядра 
+section .data
+    text db "Hello, World!", 10
  
+section .text
+    global _start
+ 
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 14
+    syscall
+ 
+    mov rax, 60
+    mov rdi, 0
+    syscall
